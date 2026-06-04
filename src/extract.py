1242c4fed060bc9837747c6c85d515f588aa0a5c
Renzo Huertas <renzo.huertas007@gmail.com>
2026-06-03 21:21:51 -0500

    Nuevos Cambios

--- src/extract.py
+++ src/extract.py
@@ -13,6 +13,7 @@ from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.options import Options
 from selenium.common.exceptions import TimeoutException
 import time
+from pathlib import Path
 import os
 
 
@@ -21,7 +22,7 @@ print("Iniciando proceso de extracción de tasas de interés...")
 ## Website de consulta
 web = "https://www.sbs.gob.pe/app/pp/EstadisticasSAEEPortal/Paginas/TIActivaTipoCreditoEmpresa.aspx?tip=B"
 
-path_raw = rf"C:\Users\PC\Desktop\Proyectos\Proyectos_Py\9.Tasas_Interes\data\raw"
+path_raw = Path(r"C:\Users\PC\Desktop\Proyectos\Proyectos_Py\9.Tasas_Interes\data\raw")
 
 ## rango de fechas
 fecha_inicio = "2026-01-05"
@@ -37,7 +38,7 @@ fecha_consulta = []
 
 for fecha in fechas:
 
-    periodo = fecha.strftime("%Y%m")   # YYYYMM
+    periodo = fecha.strftime("%Y%m")
     fecha_str = fecha.strftime("%d_%m_%Y")
 
     nombre_archivo = f"tasa_interes_{fecha_str}_{periodo}.xlsx"
@@ -52,24 +53,7 @@ for fecha in fechas:
 print("Fecha a Extraer:", fecha_consulta[-1]['fecha'])
     
 
-## Configuracion del driver
-chrome_options = Options()
-chrome_options.add_experimental_option("prefs", {
-    "download.default_directory": path_raw,
-    "download.prompt_for_download": False,
-    "download.directory_upgrade": True,
-    "safebrowsing.enabled": True
-})
-
-driver = webdriver.Chrome(options=chrome_options)
-wait = WebDriverWait(driver, 20)
-
-url = "https://www.sbs.gob.pe/app/pp/EstadisticasSAEEPortal/Paginas/TIActivaTipoCreditoEmpresa.aspx?tip=B"
-driver.get(url)
-
-
-
-def consulta_tasa_interes(input_fecha: str):
+def consulta_tasa_interes(driver, input_fecha: str):
 
     wait = WebDriverWait(driver, 10)
 
@@ -89,7 +73,7 @@ def consulta_tasa_interes(input_fecha: str):
 
     time.sleep(3)
 
-    # CLICK EXPORTAR (esperar que sea clickeable)}
+    # CLICK EXPORTAR (Descargar Excel)
     try:
         exportar_btn = wait.until(
             EC.presence_of_element_located((By.ID, "ctl00_cphContent_btnExportar"))
@@ -119,12 +103,12 @@ def renombrar_ultimo_archivo(path_raw, nuevo_nombre):
     rutas = []
 
     for f in archivos:
-        ruta = os.path.join(path_raw, f)
+        ruta = path_raw / f
         rutas.append(ruta)
     
     archivo_reciente = max(rutas, key=os.path.getctime)
 
-    nueva_ruta = os.path.join(path_raw, nuevo_nombre)
+    nueva_ruta = path_raw / nuevo_nombre
 
     os.rename(archivo_reciente, nueva_ruta)
 
@@ -138,9 +122,35 @@ fecha = ultima_fecha['fecha']
 periodo = ultima_fecha['periodo']
 nombre_archivo = ultima_fecha['nombre_archivo']  # ejemplo: Tasa_201901.xls
 
-print(f"Descargada  - Fecha consulta: {fecha}")
 
-descarga_ok = consulta_tasa_interes(fecha)
+ruta_archivo_final = path_raw / nombre_archivo
 
-if descarga_ok:
-    renombrar_ultimo_archivo(path_raw, nombre_archivo)
+if os.path.exists(ruta_archivo_final):
+    print(f"El archivo ya existe: {nombre_archivo}")
+    
+else:
+    print(f"Descargando archivo para fecha: {fecha}")
+    
+    ## Configuracion del driver
+    chrome_options = Options()
+    chrome_options.add_experimental_option("prefs", {
+        "download.default_directory": path_raw,
+        "download.prompt_for_download": False,
+        "download.directory_upgrade": True,
+        "safebrowsing.enabled": True
+    })
+
+    driver = webdriver.Chrome(options=chrome_options)
+    wait = WebDriverWait(driver, 20)
+
+    url = "https://www.sbs.gob.pe/app/pp/EstadisticasSAEEPortal/Paginas/TIActivaTipoCreditoEmpresa.aspx?tip=B"
+
+
+    driver.get(web)
+
+    descarga_ok = consulta_tasa_interes(driver, fecha)
+
+    if descarga_ok:
+        renombrar_ultimo_archivo(path_raw, nombre_archivo)
+    else:
+        print(f"No se descargo archivo para la fecha {fecha}")
